c08b08eb4358f84c032b2a976454ea02c3ee9e64
Yannic <yannicschnepf@gmail.com>
2022-04-06 16:08:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,308 @@
+var modules = [
+    {
+        used: 'false',
+        correct: 'Gefunden.de',
+        categorie: 'Promi',
+        question: 'Welches ist KEINE Suchmaschine',
+        answer: ['Google.de', 'fragFINN.de', 'Ecosia.de', 'Gefunden.de'],
+        explanation: 'Gemeint ist natürlich Gefunden.de!\nDiese Suchmaschine existiert bisher noch nicht.',
+    },{
+        used: 'false',
+        correct: 'Nervosität & Gereiztheit',
+        categorie: 'Sucht',
+        question: 'Wobei handelt es sich um typische Entzugssymptome bei Online-Sucht?',
+        answer: ['Nervosität & Gereiztheit', 'Augenschmerzen & Rückenprobleme', 'ADHS & Depressionen'],
+        explanation: 'Nervosität und Gereiztheit zählen zu typischen Entzugssymptomen bei Online-Sucht. Weitere Symptome können z.B. Ruhelosigkeit, oder sogar Angst- und Zwangsgedanken sein. Mit dieser richtigen Antwort verdienst Du Dir ganze'
+    },{
+        used: 'false',
+        correct: 'Youtube',
+        categorie: 'Allgemein',
+        question: 'Welche Onlineplattform gehört nicht zum Meta-Imperium(Facebook)?',
+        answer: ['Instagram', 'Youtube', 'Whatsapp'],
+        explanation: 'YouTube gehört nicht zum Meta Imperium. Seit 2006 ist YouTube ein Tochterunternehmen von Google.'
+    },{
+        used: 'false',
+        correct: 'Haltungsschäden – körperliche Folgen durch exzessives Spielen',
+        categorie: 'Sucht',
+        question: 'Eine Online-Sucht liegt dann vor, wenn über einen längeren Zeitraum hinweg bestimmte Kriterien erfüllt sind.\nWas gehört NICHT dazu?',
+        answer: ['Toleranzentwicklung – immer mehr spielen müssen', 'Kontrollverlust – häufig länger spielen als geplant', 'Fixierung – alles dreht sich um das Spiel/ „Online sein“', 'Haltungsschäden – körperliche Folgen durch exzessives Spielen'],
+        explanation: 'Toleranzentwicklung, Kontrollverlust und Fixierung sind feste Kriterien einer Suchterkrankung. Weitere Kriterien sind das Fortführen des Verhaltens trotz negativer Konsequenzen und Entzugserscheinungen. Eine mögliche Konsequenz, jedoch kein zwingendes Kriterium einer Online-Sucht, sind Haltungsschäden.'
+    },{
+        used: 'false',
+        correct: 'Exzellentes Aussehen',
+        categorie: 'Sucht',
+        question: 'Es existieren zahlreiche Schutzfaktoren, die eine Online-Sucht unwahrscheinlicher machen. Welche der hier aufgeführten Begriffe ist kein Schutzfaktor?',
+        answer: ['Fürsorgliche Familie', 'Hoher Selbstwert', 'Exzellentes Aussehen', 'Interessante Hobbys'],
+        explanation: 'Eine liebevolle und fürsorgliche Familie, interessante Hobbys und ein hoher Selbstwert können Dich vor einer Sucht schützen.'
+    },{
+        used: 'false',
+        correct: '1983',
+        categorie: 'Allgemein',
+        question: 'Wann kam das erste Mobiltelefon auf den Markt?',
+        answer: ['1990', '1996', '1968', '1983'],
+        explanation: 'Das erste Mobiltelefon brachte die Firma Motorola unter dem Namen DynaTac 8000x im Jahr 1983 auf den Markt. Lange telefonieren konnte man damit trotz seines stolzen Preises von fast 4000 Dollar aber nicht, denn der Akku hielt nur etwa 30 Minuten. Länger hätte man es bei einem Gewicht von 800 Gramm sowieso nicht ans Ohr halten können.'
+    },{
+        used: 'false',
+        correct: 'Das kommt ganz darauf an, ob auch andere Personen auf dem Foto sind',
+        categorie: 'Datenschutz',
+        question: 'Ist es erlaubt Fotos, die Du z.B. mit Deinem Handy gemacht hast, ohne Zustimmung ins Internet zu stellen oder über soziale Netzwerke zu verbreiten?',
+        answer: ['Klar, das Foto gehört mir. Ich darf damit machen, was ich will', 'Das kommt ganz darauf an, ob auch andere Personen auf dem Foto sind'],
+        explanation: 'Es gilt das sogenannte „Recht am eigenen Bild“, d.h. jede abgebildete Person muss vor der Veröffentlichung um Erlaubnis gefragt werden. Ausnahmen gelten, wenn auf dem Foto große Menschenmengen (z.B. Konzerte) zu sehen sind und es entsprechend nicht um die Abbildung der einzelnen Leute, sondern um die Atmosphäre geht.'
+    },{
+        used: 'false',
+        correct: 'Etwa 30 mal',
+        categorie: 'Allgemein',
+        question: 'Wie oft muss man eine Festplatte formatieren, bis alle Daten unwiederherstellbar gelöscht sind?',
+        answer: ['Etwa 30 mal', 'Zwei mal', 'Etwa 10 mal'],
+        explanation: 'Bis alle Daten unwiderruflich von einer Festplatte gelöscht sind, muss diese etwa 30 mal formatiert werden.'
+    },{
+        used: 'false',
+        correct: 'Richtig',
+        categorie: 'stimmts',
+        question: 'In Deutschland gibt es ein Bürgerrecht auf Internetnutzung!',
+        answer: ['Richtig', 'Falsch'],
+        explanation: 'Der Bundesgerichtshof (BGH) urteilte im Jahr 2013 darüber und entschied das Recht auf Internet. Die damalige Bundesjustizministerin Leutheusser-Schnarrenberger sagte: Es setzt sich die Erkenntnis durch, dass die Internetnutzung ein Bürgerrecht ist.' 
+    },{
+        used: 'false',
+        correct: 'Falsch',
+        categorie: 'stimmts',
+        question: 'Die allerersten Worte, die im Jahre 1860 über ein Draht-Telefon übermittelt worden waren: "Kein Anschluss unter dieser Nummer"',
+        answer: ['Richtig', 'Falsch'],
+        explanation: 'Das ist natürlich Unsinn. Noch unglaublicher sind aber die Worte, die Erfinder Phillip Reis damals der Überlieferung nach tatsächlich übermittelt haben soll, nämlich: „Das Pferd frisst keinen Gurkensalat“. Unglaublich, aber wahr!'
+    },{
+        used: 'false',
+        correct: 'Richtig',
+        categorie: 'stimmts',
+        question: 'Frankreich hat im Jahr 2018 ein generelles Handyverbot an Schulen eingeführt.',
+        answer: ['Richtig', 'Falsch'],
+        explanation: 'Frankreich hat im Jahr 2018 ein generelles Handyverbot an Schulen eingeführt.'
+    },{
+        used: 'false',
+        correct: 'Richtig',
+        categorie: 'stimmts',
+        question: 'Online-Sucht wurde zum ersten Mal in einem Artikel im Jahr 1995 erwähnt.',
+        answer: ['Richig', 'Falsch'],
+        explanation: 'Ivan Goldberg, ein Psychiater aus New York, schrieb im Jahr 1995 einen Artikel über Online-Sucht. Schon beachtlich, wenn man bedenkt, dass das kommerzielle Internet, so wie wir es heute kennen, gar nicht viel älter ist. Eigentlich war Goldbergs Artikel als Scherz gemeint. Dennoch nahmen sich in der Folge mehr und mehr Forscher dieses Themas an. Kein Scherz'
+    },{
+        used: 'false',
+        correct: 'Richtig',
+        categorie: 'stimmts',
+        question: 'Online-Spielsucht ist laut Weltgesundheitsorganisation (WHO) seit 2018 eine anerkannte Krankheit und Bestandteil des Diagnoseschlüssels ICD 11.',
+        answer: ['Richtig', 'Falsch'],
+        explanation: 'Seit 2018 ist die Online-Spielsucht Bestandteil des ICD 11 und als Krankheit offiziell anerkannt. Häufig werden die Begriffe Medien-Sucht, Online-Sucht und Online-Spielsucht synonym gebraucht. Tatsächlich handelt es sich allerdings um unterschiedliche Störungsbilder.'
+    },{
+        used: 'false',
+        correct: 'Auf Deine innere Uhr hören',
+        categorie: 'Sucht',
+        question: 'All diese Dinge können Dir helfen, Dein Computerspielverhalten zu kontrollieren und so einer Sucht vorzubeugen – bis auf eines, und zwar?',
+        answer: ['Jede Woche min. einen Spielfrei-Tag einrichten', 'Die eigenen Computerspielzeiten notieren', 'Auf Deine innere Uhr hören', 'Nicht am Computer essen'],
+        explanation: 'Festgelegte Pausen oder Spielfrei-Tage helfen Dir dabei zu schauen, ob Du Dein Computerspielverhalten noch bewusst kontrollieren kannst. Wenn Du Deine Online-Zeiten notierst, hast Du diese schwarz auf weiß und kannst Dich nicht so leicht selber anlügen. Das Einzige, was Dir wirklich überhaupt nicht hilft, ist Deine innere Uhr, denn sobald es im Spiel richtig spannend wird, bleibt sie förmlich stehen.'
+    },{
+        used: 'false',
+        correct: 'Akute Übelkeit',
+        categorie: 'Sucht',
+        question: 'Wobei handelt es sich um KEIN typisches Entzugssymptom bei Online-Sucht?',
+        answer: ['Leichte Reizbarkeit', 'Akute Übelkeit', 'Innere Unruhe'],
+        explanation: 'Leichte Reizbarkeit und innere Unruhe zählen zu typischen Entzugssymptomen bei Online-Sucht. Akute Übelkeit hingegen steht hiermit nicht in direktem Zusammenhang.'
+    },{
+        used: 'false',
+        correct: '13',
+        categorie: 'Datenschutz',
+        question: 'Bis zu welchem Alter zählt man gesetzlich als Kind?',
+        answer: ['13', '14', '16'],
+        explanation: 'In Deutschland zählt man nach § 1.1 JuSchG im Alter von 0-13 als Kind und im Alter von 14-17 als Jugendlicher'
+    },{
+        used: 'false',
+        correct: 'Nein, weil dieser volljährig ist',
+        categorie: 'Datenschutz',
+        question: ' Darf ein 16 jähriges Mädchen ihrem 18 jährigen Freund ein Nacktbild schicken',
+        answer: ['Ja, wenn dieser es nicht weiterschickt', 'Nein, weil dieser volljährig ist'],
+        explanation: 'Nein, weil dieser volljährig ist'
+    },{
+        used: 'false',
+        correct: '220000',    
+        categorie: 'Allgemein',
+        question: 'Wie viele Menschen in Deutschland sind von einer Mediensucht betroffen?',
+        answer: ['220000', '250500', '326800'],
+        explanation: 'Im Jahr 2019 litten ungefähr 220000 Menschen in Deutschland an Mediensucht.'
+    },{
+        used: 'false',
+        correct: 'bis 2 Jahre',
+        categorie: 'Datenschutz',
+        question: 'Bis zu wieviel Jahren Freiheitsstrafe kann das Fotografieren in Umkleiden und Toiletten bestraft werden?',
+        answer: ['bis 2 Jahre', 'bis 5 Jahre', 'bis 10 Monate'],
+        explanation: 'Das fotografieren in diesem höchst persönlichen Lebensbereich nicht erlaubt und stellt eine Straftat dar'
+    },{
+        used: 'false',
+        correct: '1985',
+        categorie: 'Promi',
+        question: 'Wann erschien das erste Super Mario?',
+        answer: ['1983', '1984', '1985'],
+        explanation: '„Super Mario Bros. “ erschien 1985 in Japan.'
+    },{
+        used: 'false',
+        correct: 'wenn es privat bleibt',
+        categorie: 'Datenschutz',
+        question: 'Wann ist Sexting KEINE Straftat?',
+        answer: ['wenn es weitergeschickt wird', 'wenn es privat bleibt', 'wenn es nur einem engen Freund gezeigt wird'],
+        explanation: 'Sexting ist keine Straftat, solange es einvernämlich passiert und die ausgetauschten Medien nicht an Dritte weitergegeben werden.'
+    },{
+        used: 'false',
+        correct: 'Tim Berners-Lee',
+        categorie: 'Promi',
+        question: 'Was ist der Name des Entwicklers, der den ersten Internet Browser entwickelt hat?',
+        answer: ['Nikola Tesla', 'Tim Berners-Lee', 'Peter Griffin'],
+        explanation: 'Tim Berners-Lee hat den ersten funktionablen Internet Browser entwickelt.'
+    },{
+        used: 'false',
+        correct: 'Facebook',
+        categorie: 'Promi',
+        question: 'Welche ist die meist benutzte Plattform (2022 - Statista)?',
+        answer: ['Whatsapp', 'Facebook', 'Instagram'],
+        explanation: 'Facebook ist die meist benuzte Plattform.'
+    },{
+        used: 'false',
+        correct: 'Vier',
+        categorie: 'Promi',
+        question: 'Wie viele Oktette befinden sich in einer IPv4 Adresse?',
+        answer: ['Acht', 'Zwei', 'Vier'],
+        explanation: 'Es befinden sich Vier Oktette in einer IPv4 Adresse.'
+    },{
+        used: 'false',
+        correct: 'Echtzeit Browsergames',
+        categorie: 'Allgemein',
+        question: 'Das Suchtpotential von welchem Spiel-Genres ist am größten?',
+        answer: ['RTS(Real-Time-Strategy', 'First-Person-Shooter', 'Echtzeit Browsergames'],
+        explanation: 'Ähnlich wie bei Online-Rollenspielen, handelt es sich auch bei Echtzeit Browsergames um so genannte „konstante Onlinewelten“. Das Besondere ist, dass diese Welten in der Regel kein vorgegebenes Ende haben und zudem auch weiterlaufen, wenn man sich nicht einloggt. Der Aufforderungscharakter, sich so oft wie möglich einzuloggen und somit auch das Suchtpotential, ist hier daher besonders hoch.'
+    }
+];
+
+
+let scoreOne = 0;
+let scoreTwo = 0;
+let turn = true;
+document.getElementById('score').innerHTML = 'Team 1: ' + scoreOne + '      |      Team 2: ' + scoreTwo;
+
+
+
+const buttonClicked = (clickedName) => {
+    document.getElementById("main").style.display = "none";
+    clickedName.disabled = true;
+    const name = clickedName.className;
+    var elements = [];
+    
+
+    
+    var question;
+
+    if (name.includes("stimmts")) {
+        for (let i = 0; i < modules.length; i++) {
+            if (modules[i].categorie.includes("stimmts")) {
+                elements.push(modules[i]);
+            }
+        }
+    } else if (name.includes("sucht")) {
+        for (let i = 0; i < modules.length; i++) {
+            if (modules[i].categorie.includes("Sucht")) {
+                elements.push(modules[i]);
+            }
+        }
+    } else if (name.includes("datenschutz")) {
+        for (let i = 0; i < modules.length; i++) {
+            if (modules[i].categorie.includes("Datenschutz")) {
+                elements.push(modules[i]);
+            }
+        }
+    } else if (name.includes("allgemein")) {
+        for (let i = 0; i < modules.length; i++) {
+            if (modules[i].categorie.includes("Allgemein")) {
+                elements.push(modules[i]);
+            }
+        }
+    } else if (name.includes("promi")) {
+        for (let i = 0; i < modules.length; i++) {
+            if (modules[i].categorie.includes("Promi")) {
+                elements.push(modules[i]);
+            }
+        }
+    }
+        while (true) {
+            var index = Math.floor(Math.random() * elements.length);
+
+            if (elements[index].used != "true") {
+                question = elements[index];
+                elements[index].used = "true";
+                break;
+            }
+        }
+
+        for (let i = 0; i < question.answer.length; i++) {
+            var button = document.createElement("button");
+            var text = document.createTextNode(question.answer[i]);
+
+            button.classList.add("btn");
+            button.classList.add("btn-primary");
+            button.classList.add("newButtons");
+            button.style.marginRight = "2%";
+            button.style.marginTop = "1%";
+            button.onclick = function() { newButtons(question, this, clickedName) }
+            button.appendChild(text);
+
+            document.getElementById("canvas").appendChild(button);
+        }
+        document.getElementById("test").innerHTML = question.question;
+}
+
+let isCorrect = false;
+
+function newButtons(givenModule, button, parentButton) {
+    const elements = document.getElementsByClassName("newButtons");
+
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+
+    if (button.innerText === givenModule.correct) {
+        isCorrect = true;       
+        document.getElementById("test").innerHTML = "Frage wurde richtig beantwortet\n\n" + givenModule.explanation;
+       // document.getElementById("test").innerHTML = givenModule.explanation;
+
+    } else {
+        isCorrect = false;
+        document.getElementById("test").innerHTML = "Frage wurde falsch beantwortet\n\n" + givenModule.explanation;
+       // document.getElementById("test").innerHTML = givenModule.explanation;
+    }
+    
+
+
+    var button = document.createElement("button");
+    var text = document.createTextNode("Zurück");
+    button.classList.add("btn");
+    button.classList.add("backButton");
+    button.onclick = function() {
+        document.getElementById("main").style.display = "block";
+        document.getElementById("test").innerHTML = "";
+        const elements = document.getElementsByClassName("backButton");
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    };
+    button.appendChild(text);
+    
+    document.getElementById("canvas").appendChild(button);    
+
+    if(isCorrect && turn) {
+        scoreOne += parseInt(parentButton.innerText);
+    }else if(isCorrect && !turn) {
+        scoreTwo += parseInt(parentButton.innerText);
+    }
+
+
+    document.getElementById('score').innerHTML = 'Team 1: ' + scoreOne + '      |      Team 2: ' + scoreTwo;
+    isCorrect = false;
+    turn = !turn;
+}
+
+function displayExplenation() {
+    alert("clicked");
+}
